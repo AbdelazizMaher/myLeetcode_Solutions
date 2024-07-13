78f7aba770b79f69771bed6da17860ba49cd8815
ABDELAZIZ MAHER <abdelazizmaher17499@gmail.com>
2024-07-13 18:58:56 +0300

    Runtime: 8 ms Memory Usage: 11.2 MB

--- Longest Substring Without Repeating Characters/Solution.cpp
+++ Longest Substring Without Repeating Characters/Solution.cpp
@@ -4,18 +4,18 @@ public:
         int maxLength = 0 , left = 0 , right = 0 , strLength =  s.length();
         unordered_map<char,int> charFreq {};
 
+        auto isValid = [&](){
+            for( auto [key, freq] : charFreq )
+            {
+                if( freq > 1 )
+                    return false;
+            }
+            return true;
+        };
+        
         while( right < strLength )
         {
             charFreq[s[right]]++; 
-
-            auto isValid = [&](){
-                for( auto [key, freq] : charFreq )
-                {
-                    if( freq > 1 )
-                        return false;
-                }
-                return true;
-            };
 
             while( left < right && !isValid() )
             {
