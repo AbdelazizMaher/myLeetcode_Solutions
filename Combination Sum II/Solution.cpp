4d91ecdea0ab076b08f294bcc2f65472ae6e25a0
ABDELAZIZ MAHER <abdelazizmaher17499@gmail.com>
2024-08-15 21:25:27 +0300

    Combination Sum II

---
+++ Combination Sum II/Solution.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
+        vector<vector<int>> combinations;
+        vector<int> prefix;
+
+        candidates.erase(remove_if(candidates.begin(), candidates.end(),
+        [target](const int& val) {
+            return val > target;
+        }),
+        candidates.end());
+        sort(candidates.begin(), candidates.end());
+
+        backtrack(candidates, combinations, prefix, 0, target);
+        return combinations;
+    }
+
+    void backtrack(vector<int>& candidates, vector<vector<int>>& combinations, vector<int>& prefix, int startIndex, int target) {
+        if( target == 0 ) combinations.push_back(prefix);
+        else
+        {
+            for( int i = startIndex ; i < candidates.size() ; ++i )
+            {
+                if( candidates[i] > target ) break;
+                if( i > startIndex && candidates[i] == candidates[i - 1] ) continue;
+                
+                prefix.push_back(candidates[i]);
+                backtrack(candidates, combinations, prefix, i + 1, target - candidates[i]);
+                prefix.pop_back();
+            }
+        }
+    }
+};
